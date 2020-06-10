9a6626ce4642b88d7d164197b0c69e0c4965635f
dg0397 <rgg0397@gmail.com>
2020-06-09 22:56:54 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './main.scss';
+import "./static/images/favicon-32x32.png";
+import "./static/images/icon-calculator.svg";
+import "./static/images/icon-karma.svg";
+import "./static/images/icon-supervisor.svg";
+import "./static/images/icon-team-builder.svg";
+
